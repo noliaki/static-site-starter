2e1a49e53fde17ec51c920c330f48b223f06ac8a
noliaki <noliaki@gmail.com>
2017-05-08 14:21:26 +0900

    :tada: typescript version

--- imagemin.js
+++ imagemin.js
@@ -1,18 +1,16 @@
 const path = require('path')
 const imagemin = require('imagemin')
-const imageminMozjpeg = require('imagemin-mozjpeg')
-const imageminPngquant = require('imagemin-pngquant')
+// const imageminMozjpeg = require('imagemin-mozjpeg')
+// const imageminPngquant = require('imagemin-pngquant')
 
-const srcPath = path.resolve('./src/webroot/')
-const distPath = process.env.NODE_ENV !== 'production' ? path.resolve('./dist-dev/') : path.resolve('./dist/')
+const srcPath = path.resolve('./src/webroot/img')
+const distPath = path.resolve(process.env.DIST_DIR || 'dist-dev/img')
 
-imagemin([`${srcPath}/**/*.{jpg,gif,png}`], distPath, {
-  useFolderStructure: true,
-  removePath: srcPath,
-  plugins: [
-    imageminMozjpeg(),
-    imageminPngquant({quality: '65-80'})
-  ]
+imagemin([`${srcPath}/**/*.{jpg,gif,png}`], `${distPath}/img`, {
+  // plugins: [
+  //   imageminMozjpeg(),
+  //   imageminPngquant({quality: '65-80'})
+  // ]
 }).then(files => {
   console.log(files)
 }, error => {
