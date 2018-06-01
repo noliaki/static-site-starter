bb81f9bb93da43b5c3f1fc352c0625ff152d3793
noliaki <noliaki@gmail.com>
2018-06-02 00:44:56 +0900

    :+1: add prettier

--- src/docroot/ts/index.ts
+++ src/docroot/ts/index.ts
@@ -1,2 +1,4 @@
 import '@/sns-share'
-const hoge = 'hoge'
+const hoge = "'hoge'"
+
+const fuga = [1, 3, 4, 5, 3, 23, 23]
--- src/modules/ts/sns-share.ts
+++ src/modules/ts/sns-share.ts
@@ -2,5 +2,9 @@ function onClick (event) {
   event.preventDefault()
 
   const href = event.currentTarget.getAttribute('href')
-  window.open(href, 'share this page', 'width=550, height=450, personalbar=0, toolbar=0, scrollbars=1, resizable=!')
+  window.open(
+    href,
+    'share this page',
+    'width=550, height=450, personalbar=0, toolbar=0, scrollbars=1, resizable=!'
+  )
 }
